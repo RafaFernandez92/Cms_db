a0f86b24e93583254325b603c020825cbe94f718
RafaFernandez92 <rafa.8nov92@gmail.com>
2024-03-26 15:51:53 +0000

    Inserting user inputs and polishing SQL commands

--- src/cms_db/Cms_db.java
+++ src/cms_db/Cms_db.java
@@ -4,6 +4,8 @@
  */
 package cms_db;
 
+import java.util.Scanner;
+
 /**
  *
  * @author User
@@ -14,7 +16,9 @@ public class Cms_db {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner userINPUT = new Scanner(System.in);
+        
+        
     }
     
 }
--- src/cms_db/JavaConnector.java
+++ src/cms_db/JavaConnector.java
@@ -4,6 +4,11 @@
  */
 package cms_db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+
 /**
  *
  * @author User
@@ -13,6 +18,12 @@ public class JavaConnector {
     private final String USER = "admin";
     private final String PASSWORD = "java";
     
+    public void courseReport() throws SQLException {
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();
+            stmt.execute("use database db_cms;");
+            stmt.execute("select g.module_name," +" count c.no_students as total_no_students" + "from grades g" + "join l.lecturer_name as lecturers" + "join l.lecturer_subject as programme";);
         }
     }
 }
