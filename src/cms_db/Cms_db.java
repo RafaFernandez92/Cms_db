ddd1744207c0d1e09f9362854188b0aacf6d7e34
RafaFernandez92 <rafa.8nov92@gmail.com>
2024-03-26 19:24:00 +0000

    Testing a kind of interface

--- src/cms_db/Cms_db.java
+++ src/cms_db/Cms_db.java
@@ -16,9 +16,26 @@ public class Cms_db {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner userINPUT = new Scanner(System.in);
+        int userINPUT;
+        
+        System.out.println("Welcome to the college Database.");
+        System.out.println("1. Login.\n" + "2. Exit.");
+        Scanner userANSWER = new Scanner(System.in);
+        try{
+            userINPUT = userANSWER.nextInt();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+        if(userINPUT == 1){
+            System.out.println("Introduce your credentials: ");
+            System.out.println("Username: " + userINPUT);
+            System.out.println("Password: " + userINPUT);
+        }else {
+            
+        }
         
         
+      
     }
     
 }
